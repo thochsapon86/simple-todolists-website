d03154a037eaf0066696ba489441b0bca2223ce3
thochsapon <candytf822@gmail.com>
2023-10-21 16:02:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ function addTaskList() {
 
     if ( inputTask != "") {
         console.log(inputTask)
+        //สร้าง list 
         const listItem = document.createElement("li");
         listItem.innerHTML = `<input type='checkbox'> ${inputTask}<button>remove</button>`;
+        //ลบ list
         const deleteButton = listItem.querySelector("button");
         deleteButton.addEventListener("click", function () {
             listItem.remove();
         });
-
+        //แสดง list
         todoList.appendChild(listItem);
-
+        //ทำให้ค่า input ว่าง
         document.getElementById("inputTask").value = ""
     } 
     else {
